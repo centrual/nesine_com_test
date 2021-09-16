9e15179db4fded1ec080520ff7e8f9f255b0e473
Oğuz Özcan <oguz@tetahq.com>
2021-09-16 16:53:22 +0300

    Build problemi giderildi

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack5: true,
-  experimental: true,
-  compress: true,
   poweredByHeader: false,
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -61,6 +61,7 @@ const Index: NextPage = () => {
 
   useEffect(() => {
     loadEvents()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
